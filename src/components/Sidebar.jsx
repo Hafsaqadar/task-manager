2c8cd2ea656884ea3cbb62efaaa8fc02432bfe36
hafsaqadar <haf252537@gmail.com>
2025-02-16 23:16:42 +0200

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,7 @@ const Sidebar = () => {
     <div className={` ${open ? 'md:w-72 ' : 'sm:w-20'} bg-white h-screen shadow-lg p-6 rounded relative sm:w-20  transition-all duration-300 ease-in-out`}>
 
   
-     {open &&<h1 className="text-lg font-bold mb-8 w-28 sm:block hidden">Do-it</h1>}
+     {open &&<h1 className=" text-3xl font-bold mb-8 w-28 sm:block hidden">Do-it</h1>}
       <span
        onClick={() => setOpen(!open)} className={`bg-purple-800 absolute right-3 top-9 cursor-pointer w-7
       rounded-full px-2 text-xl text-white mr-4 ${ open ? '' : 'sm:block' } `}
