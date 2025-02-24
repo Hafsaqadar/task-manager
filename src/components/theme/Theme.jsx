dc5bdcbcdee2f433a69e43d2f85b5be7c60d1d01
hafsaqadar <haf252537@gmail.com>
2025-02-24 15:32:32 +0200

    deployed

---
+++ src/components/theme/Theme.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+import './theme.css'
+
+
+const Theme = () => {
+  const switchTheme = (e) => {
+    if(e.target.checked){
+      document.querySelector('body').setAttribute('data-theme', 'dark')
+    } else{
+      document.querySelector('body').setAttribute('data-theme', 'light')
+    }
+  }
+  return (
+    <>
+    <div > 
+   <label className="switch">
+  <input type="checkbox"  onChange={switchTheme}/>
+  <span className="slider round"></span>
+ 
+   </label>
+  
+</div>
+
+</>  )
+}
+
+export default Theme
